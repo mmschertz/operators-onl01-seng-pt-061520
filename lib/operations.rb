9ee24b4480bb064d27937691fa3735e84d662235
Megan Schertz <meganschertz@gmail.com>
2020-06-18 22:38:46 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,7 +3,7 @@ require 'pry'
 def unsafe?(speed)
   if speed < 40 || speed > 60
     return true
-    if 40 < speed && speed < 60
+    elsif 40 < speed && speed < 60
       return false
       binding.pry
  end
